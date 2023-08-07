ae541a1e2554a1090330dd8ade7aac962a8a59da
junguk <junkuk9503@gmail.com>
2023-08-07 22:52:03 +0900

    rebase/merge to commit

--- app/src/main/java/com/example/usw_random_chat/MainActivity.kt
+++ app/src/main/java/com/example/usw_random_chat/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.usw_random_chat.Screen.LoadingScreen
 import com.example.usw_random_chat.ui.theme.USW_Random_ChatTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    Greeting("Android")
                 }
             }
         }

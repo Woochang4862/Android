1f8a3bcb59c1db7adcf237f88de47440606c2fbe
junguk03 <97594871+junguk03@users.noreply.github.com>
2023-08-16 23:06:14 +0900

    Main (#6)
    * feat : Signin 화면 완성
    
    * Feedback 화면
    
    * rebase/merge to commit
    
    * UI : 회원가입 UI 메인화면.
    
    * merge 완료
    
    * .
    
    * .
    
    * .
    
    * UI : 회원가입 UI 메인화면.
    
    * .
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add : xml
    
    * feat : 비밀번호 찾기 화면 완성
    
    * add : compose viewmodel 디펜던시 추가
    
    * mod : viewmodel 작업중(미완)
    
    * Mod : State, fontfamily
    
    * Add : image, Mod : State, fontfamily
    
    * Add : image
    
    * pull commit
    
    * add : .idea
    
    * feat: 닉네임 중복확인 추가
    
    * UI : 비밀번호 변경
    
    * UI : 회원가입화면
    
    * feat: 커스텀 버튼 재활용 가능하게 위젯으로 추가
    
    * mod : 버튼을 재활용하여 변경
    
    * feat : IdSearch 셍성
    
    ---------
    
    Co-authored-by: 이경수 <64278039+Lee-GS@users.noreply.github.com>
    Co-authored-by: Yong Hyun <0203jyh00@naver.com>
    Co-authored-by: Lee-GS <leegs4831@naver.com>

---
+++ app/src/main/java/com/example/usw_random_chat/Screen/IdSearch.kt
@@ -0,0 +1,250 @@
+package com.example.usw_random_chat.Screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.usw_random_chat.R
+
+@Composable
+fun IdSearch(){
+    val editTextState = remember {
+        mutableStateOf("")
+    }
+    val openFlag = remember {
+        mutableStateOf(false)
+    }
+    if (editTextState.value.length>6){
+        openFlag.value = true
+    }
+    if (editTextState.value.length<=6){
+        openFlag.value = false
+    }
+    IdSearchText()
+    IdSearchEmail(textState = editTextState)
+    IdSearchEmailBtn(openFlag.value)
+    IdSearchExitBtn()
+}
+
+
+@Composable
+fun IdSearchExitBtn(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = 32.dp,
+                top = 80.dp
+            )
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.back),
+            contentDescription = "image description",
+            modifier = Modifier
+                .width(36.dp)
+                .height(36.dp)
+        )
+    }
+}
+
+@Composable
+fun IdSearchText(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = 155.dp,
+                top = 88.dp
+            )
+    ) {
+        Text(
+            text = "아이디 찾기",
+            color = Color(0xFF111111),
+            style = TextStyle(
+                fontFamily = FontFamily(Font(R.font.pretendard_regular))
+            ),
+            fontSize = 18.sp,
+            fontWeight = FontWeight(600)
+        )
+    }
+}
+
+@Composable
+fun IdSearchEmail(textState: MutableState<String>) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = 31.dp,
+                top = 143.dp
+            )
+    ) {
+        TextField(
+            value = textState.value,
+            onValueChange = {textStateValue -> textState.value = textStateValue},
+            placeholder = {
+                Text(
+                    "포털 이메일 입력",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.pretendard_regular))
+                    )
+                )
+            },
+            trailingIcon = {
+                Text(
+                    "@suwon.ac.kr   ",
+                    color = Color(0xFF000000))
+            },
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+            modifier = Modifier
+                .width(326.dp)
+                .height(56.dp),
+        )
+    }
+}
+
+@Composable
+fun IdSearchEmailBtn(flag: Boolean){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                top = 256.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color(0xFF989898))) {
+                    append("*입력하신 메일로 ")
+                }
+                withStyle(style = SpanStyle(color = Color(0xFF2D64D8))) {
+                    append("가입된 아이디 ")
+                }
+                withStyle(style = SpanStyle(color = Color(0xFF989898))){
+                    append("정보를 전송합니다")
+                }
+            },
+            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+            fontSize = 12.sp,
+            color = Color(0xFFDCDCDC),
+            modifier = Modifier
+                .height(18.dp)
+                .width(242.dp)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button(
+            modifier = Modifier
+                .width(326.dp)
+                .height(56.dp),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xFF2D64D8)
+            ),
+            onClick = {},
+            enabled = flag
+        ) {
+            Text(
+                text = "확인메일 전송",
+                fontWeight = FontWeight(600),
+                color = Color(0xFFFFFFFF),
+                fontSize = 18.sp,
+                style = TextStyle(
+                    fontFamily = FontFamily(Font(R.font.pretendard_regular))
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button(
+            modifier = Modifier
+                .width(326.dp)
+                .height(56.dp),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xFF111111)
+            ),
+            onClick = {}
+        ) {
+            Text(
+                text = "로그인 하러가기",
+                fontWeight = FontWeight(600),
+                color = Color(0xFFFFFFFF),
+                fontSize = 18.sp,
+                style = TextStyle(
+                    fontFamily = FontFamily(Font(R.font.pretendard_regular))
+                )
+            )
+        }
+    }
+}
+
+
+
+
+
+@Preview (showBackground = true)
+@Composable
+fun IdSerchPreview(){
+    IdSearch()
+}
+
+@Preview (showBackground = true)
+@Composable
+fun IdSearchTextPreview(){
+    IdSearchText()
+}
+
+
+@Preview (showBackground = true)
+@Composable
+fun IdSearchExitBtnPreview() {
+    IdSearchExitBtn()
+}

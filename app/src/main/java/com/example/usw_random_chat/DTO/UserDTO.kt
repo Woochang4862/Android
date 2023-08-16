b59ec97097d071a26e5cfd669464ca4635b10812
Lee-GS <leegs4831@naver.com>
2023-08-16 23:44:28 +0900

    feat : UserDTO 추가

---
+++ app/src/main/java/com/example/usw_random_chat/DTO/UserDTO.kt
@@ -0,0 +1,10 @@
+package com.example.usw_random_chat.DTO
+
+data class UserDTO(
+    val id : String = "",
+    val password : String = "",
+    val email : String = "",
+    val nickname : String = "",
+    val mbti : String = "",
+    val selfIntroduce : String = ""
+)

c54dacbf435bfa3e3a1911fc92ffb1858db22af2
Lee-GS <leegs4831@naver.com>
2023-08-18 00:40:21 +0900

    ui : 메인페이지 완성(drawable은 미완성)

---
+++ app/src/main/java/com/example/usw_random_chat/Screen/MainPage.kt
@@ -0,0 +1,203 @@
+package com.example.usw_random_chat.Screen
+
+import android.annotation.SuppressLint
+import android.text.style.IconMarginSpan
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.usw_random_chat.R
+import com.example.usw_random_chat.ui.copyRightByFlag
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = { MyTopAppBar() },
+        content = { MainContents() }
+    )
+}
+
+@Composable
+fun MyTopAppBar() {
+    TopAppBar(
+        title = {
+            Image(
+                painter = painterResource(id = R.drawable.suchat),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(76.dp)
+                    .height(20.dp)
+            )
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Menu",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .width(37.dp)
+                        .height(30.dp)
+
+                )
+            }
+        },
+        modifier = Modifier
+            .height(80.dp),
+        backgroundColor = Color.White
+    )
+}
+
+@Composable
+fun MainContents() {
+    TalkBalloon()
+    AdBanner()
+    MainText()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        MatchingButton()
+        subText()
+        copyRightByFlag(modifier = Modifier.padding(top = 120.dp))
+    }
+
+}
+
+@Composable
+fun AdBanner() {
+    Text(
+        text = "배너광고가 들어올곳",
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Gray)
+            .height(55.dp)
+    )
+}
+
+@Composable
+fun MainText() {
+    Column {
+        Text(
+            text = "재미있고 편하게\n" +
+                    "마음껏 대화 해보세요",
+            fontSize = 28.sp,
+            lineHeight = 36.sp,
+            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+            fontWeight = FontWeight(600),
+            color = Color(0xFF111111),
+            modifier = Modifier
+                .padding(top = 123.dp, start = 32.dp)
+                .height(72.dp)
+        )
+        Text(
+            text = "마음이 통하는 수원대학교 친구들과\n" +
+                    "무엇이든 이야기 해보세요",
+            fontSize = 16.sp,
+            lineHeight = 22.sp,
+            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+            fontWeight = FontWeight(500),
+            color = Color(0xFF767676),
+            modifier = Modifier
+                .padding(top = 18.dp, start = 32.dp)
+                .height(44.dp)
+        )
+    }
+
+}
+
+@Composable
+fun TalkBalloon() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 177.dp, top = 22.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.talkballoon2),
+            contentDescription = "image description",
+            modifier = Modifier
+                .width(331.dp)
+                .height(308.dp),
+        )
+    }
+}
+
+@Composable
+fun MatchingButton() {
+    Button(
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFF2D64D8),
+            contentColor = Color.White
+        ),
+        shape = RoundedCornerShape(25.dp),
+        modifier = Modifier
+            .padding(top = 400.dp)
+            .width(334.dp)
+            .height(64.dp)
+    ) {
+        Text(
+            text = "매칭 시작하기",
+            fontSize = 24.sp,
+            fontFamily = FontFamily(Font(R.font.kcc_chassam)),
+            lineHeight = 24.sp,
+            fontWeight = FontWeight(400),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun subText() {
+    Text(
+        text = "주의!! 욕설 및 상대방에게 불쾌함을 주는 채팅\n" +
+                "적발 시 계정 이용이 제한됩니다 ",
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontFamily = FontFamily(Font(R.font.kcc_chassam)),
+        fontWeight = FontWeight(400),
+        color = Color(0xFF767676),
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .padding(top = 24.dp)
+
+        )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}

8b750c7a0fe6fe9b723ee6cb35683adbae15a5fc
Lee-GS <leegs4831@naver.com>
2023-08-30 16:31:01 +0900

    feat : 화면 이동을 위한 파일들

---
+++ app/src/main/java/com/example/usw_random_chat/Screen/Navigation.kt
@@ -0,0 +1,65 @@
+package com.example.usw_random_chat.Screen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.LoadingScreen.route
+    )
+    {
+        composable(route = Screen.SignInScreen.route) {
+            SignInScreen()
+        }
+        composable(route = Screen.SignUpScreen.route) {
+            SignUpScreen()
+        }
+        composable(route = Screen.SignUpDoneScreen.route) {
+            SignUpDoneScreen()
+        }
+        composable(route = Screen.PwChangeScreen.route) {
+            PwChangeScreen()
+        }
+        composable(route = Screen.PwSearchScreen.route) {
+            PwSearchScreen()
+        }
+        composable(route = Screen.IdSearchScreen.route) {
+            IdSearch()
+        }
+        composable(route = Screen.ProfileScreen.route) {
+            ProfileScreen()
+        }
+        composable(route = Screen.FeedBackScreen.route) {
+            FeedbackShow()
+        }
+        composable(route = Screen.PolicyScreen.route) {
+            PolicyScreen()
+        }
+        composable(route = Screen.EditProfileScreen.route) {
+            EditProfileScreen()
+        }
+        composable(route = Screen.LoadingScreen.route) {
+            LoadingScreen()
+        }
+        composable(route = Screen.ChatScreen.route) {
+            ChattingScreen()
+        }
+        composable(route = Screen.MainPageScreen.route) {
+            MainScreen()
+        }
+        composable(route = Screen.MatchingScreen.route) {
+            MainScreen()
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/usw_random_chat/Screen/Screen.kt
@@ -0,0 +1,19 @@
+package com.example.usw_random_chat.Screen
+
+sealed class Screen(val route: String){
+    object SignInScreen: Screen("SignIn")
+    object SignUpScreen: Screen("SignUp")
+    object SignUpDoneScreen: Screen("SignUp_Done")
+    object LoadingScreen: Screen("Loading")
+    object IdSearchScreen: Screen("IdSearch")
+    object PwSearchScreen: Screen("PwSearch")
+    object PwChangeScreen: Screen("PwChange")
+    object ProfileScreen: Screen("Profile")
+    object PolicyScreen: Screen("Policy")
+    object FeedBackScreen: Screen("Feedback")
+    object EditProfileScreen: Screen("EditProfile")
+    object ChatScreen: Screen("Chat")
+    object MainPageScreen: Screen("MainPage")
+    object MatchingScreen: Screen("Matching")
+
+}

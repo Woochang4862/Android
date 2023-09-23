bbe3235107b28e28c6ebd9810daf54cd2f09c875
Lee-GS <leegs4831@naver.com>
2023-09-24 01:12:34 +0900

    add : retrofit 코드 생성

---
+++ app/src/main/java/com/example/usw_random_chat/Backend/Register.kt
@@ -0,0 +1,30 @@
+package com.example.usw_random_chat.Backend
+
+import com.example.usw_random_chat.DTO.UserDTO
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface Register {
+    @POST("") // 세부 주소
+    fun registerSignIn(@Body jsonpath: UserDTO) : Call<UserDTO>
+
+    companion object{
+        private const val BASE_URL = "" // 공통 주소
+
+        fun create(): Register {
+
+            val gson : Gson = GsonBuilder().setLenient().create()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(Register::class.java)
+        }
+    }
+}

2c27c26152462ab42bd698d391fb34a170ac6296
Lee-GS <leegs4831@naver.com>
2023-09-28 00:42:24 +0900

    mod : 변수명 변경

--- app/src/main/java/com/example/usw_random_chat/DTO/UserDTO.kt
+++ app/src/main/java/com/example/usw_random_chat/DTO/UserDTO.kt
@@ -1,10 +1,10 @@
 package com.example.usw_random_chat.DTO
 
 data class UserDTO(
-    val id : String = "",
-    val password : String = "",
-    val email : String = "",
-    val nickname : String = "",
-    val mbti : String = "",
-    val selfIntroduce : String = ""
+   //val id : String = "",
+    val memberPassword : String = "",
+    val memberEmail : String = "",
+    val memberName : String = "",
+    //val mbti : String = "",
+   // val selfIntroduce : String = ""
 )

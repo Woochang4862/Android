c275173e975c5ac57393df1df90449cdadb8433a
Lee-GS <leegs4831@naver.com>
2023-10-11 22:28:01 +0900

    mod : 통신 세부 사항 볼 수 있게 수정

--- app/src/main/java/com/example/usw_random_chat/Backend/Register.kt
+++ app/src/main/java/com/example/usw_random_chat/Backend/Register.kt
@@ -3,24 +3,39 @@ package com.example.usw_random_chat.Backend
 import com.example.usw_random_chat.DTO.UserDTO
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface Register {
-    @POST("/member/save") // 세부 주소
-    fun registerSignIn(@Body jsonpath: UserDTO) : Call<UserDTO>
+    @POST("member/save") // 세부 주소
+    @Headers("content-type: application/json")
+    fun registerSignUp(@Body jsonpath: UserDTO) : Call<UserDTO>
 
     companion object{
-        private const val BASE_URL = "http://localhost:8081" // 공통 주소
+        private const val BASE_URL = "http://3.35.83.91:8080/" // 공통 주소
 
         fun create(): Register {
 
             val gson : Gson = GsonBuilder().setLenient().create()
 
-            return Retrofit.Builder()
+            //http통신 로그 기록을 볼 수 있게 해주는 코드
+            val client : OkHttpClient = OkHttpClient.Builder().apply {
+                addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+            }.build()
+
+            return Retrofit
+                .Builder()
+                .client(client)
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()

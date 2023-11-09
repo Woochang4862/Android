a8adb2bdce4b4c7a3773a8cc267841afe1bfb74c
Lee-GS <leegs4831@naver.com>
2023-11-09 21:04:42 +0900

    add: ProfileApiService 추가 (세부 주소는 아직 안나옴)

---
+++ app/src/main/java/com/example/usw_random_chat/data/api/ProfileApiService.kt
@@ -0,0 +1,14 @@
+package com.example.usw_random_chat.data.api
+
+import com.example.usw_random_chat.data.dto.ProfileDTO
+import com.example.usw_random_chat.data.dto.UserDTO
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface ProfileApiService {
+    @POST("member/??") // 세부 주소
+    @Headers("content-type: application/json")
+    suspend fun setProfile(@Body jsonpath: ProfileDTO) : Response<ProfileDTO>
+}

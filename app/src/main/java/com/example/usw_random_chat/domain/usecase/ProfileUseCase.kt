5f0e12a7754d5d2fcd6800b697a3ee7cae86cc64
Lee-GS <leegs4831@naver.com>
2023-11-09 21:03:24 +0900

    add: ProfileUseCase 추가

---
+++ app/src/main/java/com/example/usw_random_chat/domain/usecase/ProfileUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.usw_random_chat.domain.usecase
+
+import com.example.usw_random_chat.data.dto.ProfileDTO
+import com.example.usw_random_chat.data.dto.UserDTO
+import com.example.usw_random_chat.data.repository.ProfileRepository
+
+class ProfileUseCase(private val profileRepository: ProfileRepository) {
+
+    suspend fun excute(param : ProfileDTO) : ProfileDTO {
+        return profileRepository.setProfile(param)
+    }
+}

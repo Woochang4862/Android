8220cde327aa6208ee1f4bd7aec973c4615253f1
Lee-GS <leegs4831@naver.com>
2023-11-29 13:22:44 +0900

    mod : DrawerScreen 작업

--- app/src/main/java/com/example/usw_random_chat/presentation/view/MainPage.kt
+++ app/src/main/java/com/example/usw_random_chat/presentation/view/MainPage.kt
@@ -22,6 +22,7 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.material.rememberScaffoldState
@@ -31,6 +32,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
@@ -45,6 +47,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.usw_random_chat.R
 import com.example.usw_random_chat.ui.copyRightByFlag
+import com.example.usw_random_chat.ui.drawerMenu
 import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -80,9 +83,52 @@ fun MainScreen(navController: NavController) {
 
 @Composable
 fun DrawerScreen() {
-    Text(text = "!2512341")
-    Text(text = "iuvablivcasd")
-    Text(text = "!24basfibsdakjfvaisv")
+    Column(modifier = Modifier.fillMaxSize()) {
+        IconButton(
+            onClick = { /*TODO*/ },
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.cancel),
+                contentDescription = "",
+                modifier = Modifier
+                    .width(16.dp)
+                    .height(16.dp)
+            )
+        }
+        Image(
+            painter = painterResource(id = R.drawable.profile_img),
+            contentDescription = "",
+            modifier = Modifier
+                .width(49.dp)
+                .height(49.dp)
+        )
+        Text(
+            text = "AnSungMin",
+            fontSize = 22.sp,
+            lineHeight = 24.sp,
+            fontFamily = FontFamily(Font(R.font.kcc_chassam)),
+            fontWeight = FontWeight(400),
+            color = Color(0xFF111111),
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = "# ISTP",
+            fontSize = 14.sp,
+            lineHeight = 24.sp,
+            fontFamily = FontFamily(Font(R.font.kcc_chassam)),
+            fontWeight = FontWeight(400),
+            color = Color(0xFF989898),
+            textAlign = TextAlign.Center,
+        )
+        Image(
+            painter = painterResource(id = R.drawable.baseline),
+            contentDescription = "",
+            modifier = Modifier.fillMaxWidth()
+        )
+        drawerMenu(image = R.drawable.profile_img, menuName = "프로필 설정") {
+            
+        }
+    }
 }
 
 
@@ -240,4 +286,10 @@ fun subText() {
 @Composable
 fun MainScreenPreview() {
     MainScreen(navController = rememberNavController())
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DrawerScreenPreview() {
+    DrawerScreen()
 }

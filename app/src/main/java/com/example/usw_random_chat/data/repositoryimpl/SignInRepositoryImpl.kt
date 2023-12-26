591834b7f6ca72d7e15421a98bede9e79ec28e97
Lee-GS <leegs4831@naver.com>
2023-12-26 23:32:12 +0900

    add: 로그인 성공시 토큰을 내부 db에 저장

--- app/src/main/java/com/example/usw_random_chat/data/repositoryimpl/SignInRepositoryImpl.kt
+++ app/src/main/java/com/example/usw_random_chat/data/repositoryimpl/SignInRepositoryImpl.kt
@@ -2,17 +2,25 @@ package com.example.usw_random_chat.data.repositoryimpl
 
 import com.example.usw_random_chat.data.api.SignInApiService
 import com.example.usw_random_chat.data.dto.UserDTO
+import com.example.usw_random_chat.data.local.TokenSharedPreference
 import com.example.usw_random_chat.data.repository.SignInRepository
 import javax.inject.Inject
 
-class SignInRepositoryImpl @Inject constructor(private val signInApiService: SignInApiService) : SignInRepository {
+class SignInRepositoryImpl @Inject constructor(
+    private val signInApiService: SignInApiService,
+    private val tokenSharedPreference: TokenSharedPreference
+) : SignInRepository {
 
     override suspend fun signin(param: UserDTO): UserDTO {
         val response = signInApiService.registerSignIn(param)
 
-        if (response.isSuccessful){
+        if (response.isSuccessful) {
+            val accessToken = response.body()?.accessToken
+            val refreshToken = response.body()?.refreshToken
+            tokenSharedPreference.setToken("accessToken","token : $accessToken")
+            tokenSharedPreference.setToken("refreshToken","token : $refreshToken")
             return response.body()!!
-        }else{
+        } else {
             throw Exception("Fail!!")
         }
     }

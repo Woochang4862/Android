3d7f896a13c4a6252cdd0bec174085c852ac09a9
Lee-GS <leegs4831@naver.com>
2023-12-26 23:30:53 +0900

    add: token 추가

--- app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
+++ app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
@@ -3,6 +3,8 @@ package com.example.usw_random_chat.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class UserDTO(
+    val accessToken : String,
+    val refreshToken : String,
     @SerializedName("memberEmail")
     val memberEmail : String = "",
     @SerializedName("memberPW")

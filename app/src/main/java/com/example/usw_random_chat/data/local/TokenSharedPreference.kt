51aeda7265f07ceb1e6b7cd0051794cfc2391289
Lee-GS <leegs4831@naver.com>
2023-12-26 23:31:21 +0900

    add: 로컬 db 선언

--- app/src/main/java/com/example/usw_random_chat/data/local/TokenSharedPreference.kt
+++ app/src/main/java/com/example/usw_random_chat/data/local/TokenSharedPreference.kt
@@ -1,7 +1,19 @@
 package com.example.usw_random_chat.data.local
 
+import android.content.Context
+import android.content.SharedPreferences
+import java.util.prefs.Preferences
 
 
-class TokenSharedPreference  {
+class TokenSharedPreference (context: Context)  {
+    private val prefs : SharedPreferences =
+        context.getSharedPreferences("pref_name", Context.MODE_PRIVATE)
 
+    fun getToken(key: String, defValue: String): String {
+        return prefs.getString(key, defValue).toString()
+    }
+
+    fun setToken(key: String, str: String) {
+        prefs.edit().putString(key, str).apply()
+    }
 }   

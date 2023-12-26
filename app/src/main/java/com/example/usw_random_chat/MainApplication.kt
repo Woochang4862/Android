ecb8fa186ee8023ed285f8a3e0b35dd540082203
Lee-GS <leegs4831@naver.com>
2023-12-26 23:33:54 +0900

    add: onCreate시 내부 db 선언

--- app/src/main/java/com/example/usw_random_chat/MainApplication.kt
+++ app/src/main/java/com/example/usw_random_chat/MainApplication.kt
@@ -1,10 +1,18 @@
 package com.example.usw_random_chat
 
 import android.app.Application
+import com.example.usw_random_chat.data.local.TokenSharedPreference
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class MainApplication : Application() {
+    companion object {
+        lateinit var prefs: TokenSharedPreference
+    }
 
+    override fun onCreate() {
+        super.onCreate()
+        prefs = TokenSharedPreference(applicationContext)
+    }
 }

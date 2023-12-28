d5bf6cfa26c7bf29f6ab8e42fccdc10f3520d282
Lee-GS <leegs4831@naver.com>
2023-12-28 21:20:24 +0900

    add : 인증번호 변수 추가

--- app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
+++ app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
@@ -3,8 +3,9 @@ package com.example.usw_random_chat.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class UserDTO(
-    val accessToken : String,
-    val refreshToken : String,
+    val accessToken : String? = "",
+    val refreshToken : String? = "",
+    val code : String? = "",
     @SerializedName("memberEmail")
     val memberEmail : String = "",
     @SerializedName("memberPW")

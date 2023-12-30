c7f633073989483c862110d32de5e6c98a0fb08f
Lee-GS <leegs4831@naver.com>
2023-12-30 16:12:57 +0900

    mod : 로그인 성공시 메인페이지로 화면 이동

--- app/src/main/java/com/example/usw_random_chat/data/repositoryimpl/SignInRepositoryImpl.kt
+++ app/src/main/java/com/example/usw_random_chat/data/repositoryimpl/SignInRepositoryImpl.kt
@@ -1,14 +1,17 @@
 package com.example.usw_random_chat.data.repositoryimpl
 
+import androidx.navigation.NavController
 import com.example.usw_random_chat.data.api.SignInApiService
 import com.example.usw_random_chat.data.dto.UserDTO
 import com.example.usw_random_chat.data.local.TokenSharedPreference
 import com.example.usw_random_chat.data.repository.SignInRepository
+import com.example.usw_random_chat.presentation.view.Screen
 import javax.inject.Inject
 
 class SignInRepositoryImpl @Inject constructor(
     private val signInApiService: SignInApiService,
-    private val tokenSharedPreference: TokenSharedPreference
+    private val tokenSharedPreference: TokenSharedPreference,
+    private val navController: NavController
 ) : SignInRepository {
 
     override suspend fun signin(param: UserDTO): UserDTO {
@@ -19,6 +22,9 @@ class SignInRepositoryImpl @Inject constructor(
             val refreshToken = response.body()?.refreshToken
             tokenSharedPreference.setToken("accessToken","token : $accessToken")
             tokenSharedPreference.setToken("refreshToken","token : $refreshToken")
+            navController.navigate(Screen.MainPageScreen.route){
+                navController.popBackStack()
+            }
             return response.body()!!
         } else {
             throw Exception("Fail!!")

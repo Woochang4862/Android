b7e72e73de1e4e637ca8cb34170b8c02d4dde761
Lee-GS <leegs4831@naver.com>
2024-01-06 19:46:36 +0900

    mod : sharedPreference hilt 주입

--- app/src/main/java/com/example/usw_random_chat/data/local/TokenSharedPreference.kt
+++ app/src/main/java/com/example/usw_random_chat/data/local/TokenSharedPreference.kt
@@ -2,10 +2,13 @@ package com.example.usw_random_chat.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
 import java.util.prefs.Preferences
+import javax.inject.Inject
+import javax.inject.Singleton
 
-
-class TokenSharedPreference (context: Context)  {
+@Singleton
+class TokenSharedPreference @Inject constructor(@ApplicationContext context: Context)  {
     private val prefs : SharedPreferences =
         context.getSharedPreferences("tokenRepo", Context.MODE_PRIVATE)
 

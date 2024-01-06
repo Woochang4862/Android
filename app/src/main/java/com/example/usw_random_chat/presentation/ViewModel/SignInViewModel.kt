548ff9ac0d88c5548894b15048a2ce4c6bcc54de
Lee-GS <leegs4831@naver.com>
2024-01-06 19:47:58 +0900

    mod : loginState변수 추가 및 로그인시 http응답 코드에 따라 작업 변경

--- app/src/main/java/com/example/usw_random_chat/presentation/ViewModel/SignInViewModel.kt
+++ app/src/main/java/com/example/usw_random_chat/presentation/ViewModel/SignInViewModel.kt
@@ -7,19 +7,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.usw_random_chat.data.dto.UserDTO
 import com.example.usw_random_chat.domain.usecase.SignInUseCase
+import com.example.usw_random_chat.presentation.view.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor(private val signInUseCase: SignInUseCase) : ViewModel() {
+class SignInViewModel @Inject constructor(private val signInUseCase: SignInUseCase, private val navController: NavController) : ViewModel() {
 
     private val _id = mutableStateOf("")
     private val _password = mutableStateOf("")
     private val _signInValue = mutableStateOf(false)
+    private val _loginState = mutableStateOf(false)
 
+    val loginState : State<Boolean> = _loginState
     val id : State<String> = _id
     val password : State<String>  = _password
     val signInValue : State<Boolean> = _signInValue
@@ -32,10 +37,18 @@ class SignInViewModel @Inject constructor(private val signInUseCase: SignInUseCa
         _password.value = newValue
     }
 
+    fun changeLoginState(){
+        _loginState.value = !_loginState.value
+    }
 
     fun postSignIn(){
         viewModelScope.launch {//viewModelScope 공부하기
-            signInUseCase.execute(UserDTO(memberID = id.value,memberPassword = password.value))
+            when(signInUseCase.execute(UserDTO(memberID = id.value,memberPassword = password.value))){
+                !in (200..300) -> _loginState.value = true
+                in 200..300 -> navController.navigate(Screen.MainPageScreen.route){
+                    navController.popBackStack()
+                }
+            }
         }
     }
 }

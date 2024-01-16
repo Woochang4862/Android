a852dfdd162a73ea3706d2d7a353b2360dc63652
Lee-GS <leegs4831@naver.com>
2024-01-16 19:21:36 +0900

    mod: 자동로그인 기능 추가 및 코드 수정

--- app/src/main/java/com/example/usw_random_chat/data/repositoryimpl/SignInRepositoryImpl.kt
+++ app/src/main/java/com/example/usw_random_chat/data/repositoryimpl/SignInRepositoryImpl.kt
@@ -1,12 +1,15 @@
 package com.example.usw_random_chat.data.repositoryimpl
 
+import android.util.Log
 import androidx.navigation.NavController
 import com.example.usw_random_chat.data.api.SignInApiService
+import com.example.usw_random_chat.data.dto.Token
 import com.example.usw_random_chat.data.dto.UserDTO
 import com.example.usw_random_chat.data.local.TokenSharedPreference
 import com.example.usw_random_chat.data.repository.SignInRepository
 import com.example.usw_random_chat.presentation.ViewModel.SignUpViewModel
 import com.example.usw_random_chat.presentation.view.Screen
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class SignInRepositoryImpl @Inject constructor(
@@ -18,17 +21,31 @@ class SignInRepositoryImpl @Inject constructor(
     override suspend fun signIn(param: UserDTO) : Int {
         val response = signInApiService.registerSignIn(param)
 
-        if (response.isSuccessful) {
-            val accessToken = response.body()?.accessToken
-            val refreshToken = response.body()?.refreshToken
-            tokenSharedPreference.setToken("accessToken","token : $accessToken")
-            tokenSharedPreference.setToken("refreshToken","token : $refreshToken")
+        return if (response.isSuccessful) {
+            //val accessToken = response.body()?.token?.accessToken
+            //val refreshToken = response.body()?.token?.refreshToken
+            //tokenSharedPreference.setToken("accessToken","$accessToken")
+            //tokenSharedPreference.setToken("refreshToken","$refreshToken")
+            //navController.navigate(Screen.MainPageScreen.route)
+            response.code()
+        } else {
+            Log.d("로그인 실패",response.body().toString())
+            response.code()
+        }
+    }
+
+    override suspend fun autoSignIn(token: Token){
+        val response = signInApiService.autoSignIn()
+
+        if (response.isSuccessful){
             navController.navigate(Screen.MainPageScreen.route){
                 navController.popBackStack()
             }
-            return response.code()
-        } else {
-            throw Exception("Fail!!")
+        }
+        else{
+            navController.navigate(Screen.SignInScreen.route){
+                navController.popBackStack()
+            }
         }
     }
 }
--- app/src/main/java/com/example/usw_random_chat/domain/usecase/SignInUseCase.kt
+++ app/src/main/java/com/example/usw_random_chat/domain/usecase/SignInUseCase.kt
@@ -1,10 +1,15 @@
 package com.example.usw_random_chat.domain.usecase
 
+import com.example.usw_random_chat.data.dto.Token
 import com.example.usw_random_chat.data.dto.UserDTO
 import com.example.usw_random_chat.data.repository.SignInRepository
 
 class SignInUseCase(private val signInRepository: SignInRepository) {
     suspend fun execute(param : UserDTO) : Int {
         return signInRepository.signIn(param)
+    }
+
+    suspend fun autoSignIn(token: Token){
+        return signInRepository.autoSignIn(token)
     }
 }

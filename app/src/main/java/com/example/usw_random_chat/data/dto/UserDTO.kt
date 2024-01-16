e313d6ccd2245e9c28ac73c73e3baa09d5c83049
Lee-GS <leegs4831@naver.com>
2024-01-16 16:58:05 +0900

    mod: api명세서 맞게 수정(임시)

--- app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
+++ app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
@@ -3,13 +3,13 @@ package com.example.usw_random_chat.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class UserDTO(
-    val accessToken : String? = "",
-    val refreshToken : String? = "",
-    val code : String? = "",
-    @SerializedName("memberEmail")
-    val memberEmail : String = "",
-    @SerializedName("memberPw")
+    //@SerializedName("token")
+    //val token: Token? = Token("",""),
+   // val code : String? = "",
+    //@SerializedName("memberEmail")
+    //val memberEmail : String = "",
+    @SerializedName("password")
     val memberPassword : String = "",
-    @SerializedName("memberID")
+    @SerializedName("memberId")
     val memberID : String = ""
 )

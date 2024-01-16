13f2cd58542e2b32e71ed124eb5f52f1f5e45c44
Lee-GS <leegs4831@naver.com>
2024-01-16 19:19:55 +0900

    mod: dialogState 추가 및 자동로그인 기능

--- app/src/main/java/com/example/usw_random_chat/presentation/ViewModel/SignInViewModel.kt
+++ app/src/main/java/com/example/usw_random_chat/presentation/ViewModel/SignInViewModel.kt
@@ -2,30 +2,30 @@ package com.example.usw_random_chat.presentation.ViewModel
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import com.example.usw_random_chat.data.dto.Token
 import com.example.usw_random_chat.data.dto.UserDTO
+import com.example.usw_random_chat.data.local.TokenSharedPreference
 import com.example.usw_random_chat.domain.usecase.SignInUseCase
-import com.example.usw_random_chat.presentation.view.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor(private val signInUseCase: SignInUseCase, private val navController: NavController) : ViewModel() {
+class SignInViewModel @Inject constructor(private val signInUseCase: SignInUseCase, private val navController: NavController, private val pref : TokenSharedPreference) : ViewModel() {
 
     private val _id = mutableStateOf("")
     private val _password = mutableStateOf("")
     private val _loginState = mutableStateOf(false)
+    private val _dialogState = mutableStateOf(false)
+
 
     val loginState : State<Boolean> = _loginState
     val id : State<String> = _id
     val password : State<String>  = _password
+    val dialogState : State<Boolean> = _dialogState
 
     fun updateID(newValue : String){
         _id.value = newValue
@@ -35,15 +35,28 @@ class SignInViewModel @Inject constructor(private val signInUseCase: SignInUseCa
         _password.value = newValue
     }
 
+    fun changeDrawerState(){
+        _dialogState.value = !_dialogState.value
+    }
     fun changeLoginState(){
         _loginState.value = !_loginState.value
     }
-
     fun postSignIn(){
         viewModelScope.launch {//viewModelScope 공부하기
             when(signInUseCase.execute(UserDTO(memberID = id.value,memberPassword = password.value))){
-                !in (200..300) -> _loginState.value = true
+                in (200..300) -> _loginState.value = true
+                !in (200..300) -> _dialogState.value = true
             }
         }
     }
+
+    fun autoSignIn(){
+        viewModelScope.launch{
+            val accessToken = pref.getToken("accessToken","")
+            val refreshToken = pref.getToken("refreshToken","")
+
+            signInUseCase.autoSignIn(Token(accessToken,refreshToken))
+        }
+    }
+
 }

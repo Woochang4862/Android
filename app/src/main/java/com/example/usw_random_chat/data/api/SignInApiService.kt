6256aad16b13222fd0f08cb81d9787a367183520
Lee-GS <leegs4831@naver.com>
2024-01-16 16:54:17 +0900

    mod: api 주소 변경 및 자동 로그인 기능 추가

--- app/src/main/java/com/example/usw_random_chat/data/api/SignInApiService.kt
+++ app/src/main/java/com/example/usw_random_chat/data/api/SignInApiService.kt
@@ -1,13 +1,20 @@
 package com.example.usw_random_chat.data.api
 
+import com.example.usw_random_chat.data.dto.TempDTO
+import com.example.usw_random_chat.data.dto.Token
 import com.example.usw_random_chat.data.dto.UserDTO
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface SignInApiService {
-    @POST("member2/login") // 세부 주소
+    @POST("member/sign-in") // 세부 주소
     @Headers("content-type: application/json")
-    suspend fun registerSignIn(@Body jsonpath: UserDTO) : Response<UserDTO>
+    suspend fun registerSignIn(@Body jsonpath: UserDTO) : Response<TempDTO>
+
+    @POST("member/auto-sign-in")
+    suspend fun autoSignIn() : Response<Token>
 }

97ed99ac0a3f9a5937bfc65bf60ad1e240ba8257
Lee-GS <leegs4831@naver.com>
2024-01-20 15:25:50 +0900

    mod: 파라미터와 반환타입 수정

--- app/src/main/java/com/example/usw_random_chat/data/api/SignInApiService.kt
+++ app/src/main/java/com/example/usw_random_chat/data/api/SignInApiService.kt
@@ -1,20 +1,17 @@
 package com.example.usw_random_chat.data.api
 
-import com.example.usw_random_chat.data.dto.TempDTO
-import com.example.usw_random_chat.data.dto.Token
 import com.example.usw_random_chat.data.dto.UserDTO
+import com.example.usw_random_chat.data.dto.Token
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface SignInApiService {
     @POST("member/sign-in") // 세부 주소
     @Headers("content-type: application/json")
-    suspend fun registerSignIn(@Body jsonpath: UserDTO) : Response<TempDTO>
+    suspend fun registerSignIn(@Body jsonpath: UserDTO) : Response<UserDTO>
 
     @POST("member/auto-sign-in")
-    suspend fun autoSignIn() : Response<Token>
+    suspend fun autoSignIn(@Body jsonpath: Token ) : Response<Token>
 }

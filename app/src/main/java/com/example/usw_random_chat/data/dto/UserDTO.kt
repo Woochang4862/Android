2dabbfac6d207867bbefb8f6e1f895a95ab993be
Lee-GS <leegs4831@naver.com>
2024-01-20 15:27:56 +0900

    mod:

--- app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
+++ app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
@@ -3,13 +3,10 @@ package com.example.usw_random_chat.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class UserDTO(
-    //@SerializedName("token")
-    //val token: Token? = Token("",""),
-   // val code : String? = "",
-    @SerializedName("memberEmail")
-    val memberEmail : String = "",
-    @SerializedName("memberPw")
+    @SerializedName("password")
     val memberPassword : String = "",
     @SerializedName("memberId")
-    val memberID : String = ""
+    val memberID : String = "",
+    @SerializedName("token")
+    val token: Token = Token("","")
 )

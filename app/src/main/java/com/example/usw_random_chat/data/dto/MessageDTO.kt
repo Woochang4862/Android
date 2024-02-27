6cc5cb075d6c7412bdf180a4e6fdc190a34b6ebf
Lee-GS <leegs4831@naver.com>
2024-02-28 00:33:14 +0900

    add : MessageDTO 추가

---
+++ app/src/main/java/com/example/usw_random_chat/data/dto/MessageDTO.kt
@@ -0,0 +1,11 @@
+package com.example.usw_random_chat.data.dto
+
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MessageDTO(
+    val roomId : String = "",
+    val sender : String = "",
+    val contents : String = ""
+)

ef8626ff4421adbc1d1003ad81ed81c07b180a29
Lee-GS <leegs4831@naver.com>
2024-02-28 00:41:55 +0900

    mod : memberId api명세 변경

--- app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
+++ app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class UserDTO(
     @SerializedName("password")
     val memberPassword : String = "",
-    @SerializedName("memberId")
+    @SerializedName("account")
     val memberID : String = "",
     @SerializedName("email")
     val email : String = "",

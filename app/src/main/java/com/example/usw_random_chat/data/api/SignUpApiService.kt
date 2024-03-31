bdd18dceb3aab071984573fc5d41444656acdaf5
Lee-GS <leegs4831@naver.com>
2024-04-01 00:55:39 +0900

    mod : api 수정

--- app/src/main/java/com/example/usw_random_chat/data/api/SignUpApiService.kt
+++ app/src/main/java/com/example/usw_random_chat/data/api/SignUpApiService.kt
@@ -1,27 +1,34 @@
 package com.example.usw_random_chat.data.api
 
 import com.example.usw_random_chat.data.dto.UserDTO
+import com.example.usw_random_chat.data.dto.response.ResponseDTO
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface SignUpApiService {
-    @POST("member/sign-up")
+    @POST("open/member/sign-up") // 회원가입 인증 메일 전송
     @Headers("content-type: application/json")
-    suspend fun registerSignUp(@Body jsonpath: UserDTO) : Response<UserDTO>
-    @POST("member/check-duplicate-account")
+    suspend fun registerSignUp(@Body jsonpath: UserDTO) : Response<ResponseDTO>
+    @POST("open/member/check-duplicate-account") // 아이디 중복확인
     suspend fun registerIdDoubleCheck(@Body jsonpath: UserDTO) : Response<UserDTO>
 
-    @POST("member/sign-up")
+    @GET("open/member/confirm-email?uuid={uuid}") // 이메일 인증확인
+    @Headers("content-type: application/json")
+    suspend fun checkAuthEmail(@Path("uuid") uuid : String): Response<UserDTO>
+
+    @POST("open/member/reconfirm-email") // 이메일 재인증
     @Headers("content-type: application/json")
     suspend fun registerAuthEmail(@Body jsonpath: UserDTO): Response<UserDTO>
 
-    @POST("member/reconfirm-email")
+    @POST("open/member/check-duplicate-email") // 이메일 중복 확인
     @Headers("content-type: application/json")
-    suspend fun registerCheckAuthEmail(@Body jsonpath: UserDTO): Response<UserDTO>
+    suspend fun registerEmailDoubleCheck(@Body jsonpath: UserDTO): Response<UserDTO>
 
-    @POST("member/check-duplicate-nickname-signUp")
+    @POST("open/member/check-duplicate-nickname-signUp") // 닉네임 중복확인
     @Headers("content-type: application/json")
     suspend fun registerCheckSignUpNickName(@Body jsonpath: UserDTO): Response<UserDTO>
 }
--- app/src/main/java/com/example/usw_random_chat/data/api/UserModifyApiService.kt
+++ app/src/main/java/com/example/usw_random_chat/data/api/UserModifyApiService.kt
@@ -2,16 +2,19 @@ package com.example.usw_random_chat.data.api
 
 import com.example.usw_random_chat.data.dto.PassWordDTO
 import com.example.usw_random_chat.data.dto.UserDTO
+import com.example.usw_random_chat.data.dto.response.ResponseDTO
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface UserModifyApiService {
     @POST("verification/send-code") // 비밀번호 변경용 인증번호 생성
     @Headers("content-type: application/json")
-    suspend fun postCodePwSearch(@Body jsonpath: UserDTO) : Response<UserDTO>
+    suspend fun postCodePwSearch(@Body jsonpath: UserDTO) : Response<ResponseDTO>
 
     @POST("verification/send-code") // 세부 주소
     @Headers("content-type: application/json")
@@ -21,12 +24,13 @@ interface UserModifyApiService {
     @Headers("content-type: application/json")
     suspend fun checkAuthCode(@Body jsonpath: UserDTO) : Response<UserDTO>
 
-    @PATCH("verification/update-password?uuid=") // 비밀번호 변경하기
+    @PATCH("verification/update-password?uuid={uuid}") // 비밀번호 변경하기
     @Headers("content-type: application/json")
-    suspend fun changePW(@Body jsonpath: PassWordDTO) : Response<PassWordDTO>
+    suspend fun changePW(@Body jsonpath: PassWordDTO, @Header("uuid") uuid : String) : Response<PassWordDTO>
 
     @POST("member/check-duplicate-id")
     @Headers("content-type: application/json")
-    suspend fun RegisterPostCheckEmail(@Body jsonpath: UserDTO): Response<UserDTO>
+    suspend fun registerPostCheckEmail(@Body jsonpath: UserDTO): Response<UserDTO>
+
 
 }

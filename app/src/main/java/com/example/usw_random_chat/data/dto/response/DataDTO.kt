aa87fb5b255ce7df8ee3aa634af2e4361ff02a04
Lee-GS <leegs4831@naver.com>
2024-04-01 00:54:03 +0900

    mod : Response dto 추가

---
+++ app/src/main/java/com/example/usw_random_chat/data/dto/response/DataDTO.kt
@@ -0,0 +1,14 @@
+package com.example.usw_random_chat.data.dto.response
+
+import com.google.gson.annotations.SerializedName
+
+data class DataDTO(
+    @SerializedName("uuid")
+    val uuid : String = "",
+    @SerializedName("nickName")
+    val nickName : String = "",
+    @SerializedName("mbti")
+    val mbti : String? = "",
+    @SerializedName("intro")
+    val selfIntroduce : String? = ""
+)
---
+++ app/src/main/java/com/example/usw_random_chat/data/dto/response/ResponseDTO.kt
@@ -0,0 +1,6 @@
+package com.example.usw_random_chat.data.dto.response
+
+data class ResponseDTO(
+    val message : String? = "",
+    val data : DataDTO = DataDTO("","","","")
+)

cffe895ab2ba8b1434602940bc0eb913cbecc55d
Lee-GS <leegs4831@naver.com>
2024-04-05 03:21:37 +0900

    mod : companion object 위치 변경

--- app/src/main/java/com/example/usw_random_chat/MainApplication.kt
+++ app/src/main/java/com/example/usw_random_chat/MainApplication.kt
@@ -1,18 +1,22 @@
 package com.example.usw_random_chat
 
 import android.app.Application
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.usw_random_chat.data.local.TokenSharedPreference
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 
 @HiltAndroidApp
 class MainApplication : Application() {
-    companion object {
-        lateinit var prefs: TokenSharedPreference
-    }
 
     override fun onCreate() {
         super.onCreate()
         prefs = TokenSharedPreference(applicationContext)
+    }
+
+    companion object {
+        lateinit var prefs: TokenSharedPreference
     }
 }

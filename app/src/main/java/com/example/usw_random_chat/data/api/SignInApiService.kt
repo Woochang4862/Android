0a3202c62251fbb7b3f292f71bab25b7de2d8638
Lee-GS <leegs4831@naver.com>
2024-05-16 22:41:53 +0900

    mod: 주소 수정

--- app/src/main/java/com/example/usw_random_chat/data/api/SignInApiService.kt
+++ app/src/main/java/com/example/usw_random_chat/data/api/SignInApiService.kt
@@ -2,16 +2,19 @@ package com.example.usw_random_chat.data.api
 
 import com.example.usw_random_chat.data.dto.UserDTO
 import com.example.usw_random_chat.data.dto.Token
+import com.example.usw_random_chat.data.dto.response.LoginResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface SignInApiService {
-    @POST("member/sign-in") // 세부 주소
+    @POST("open/member/sign-in") // 세부 주소
     @Headers("content-type: application/json")
-    suspend fun registerSignIn(@Body jsonpath: UserDTO) : Response<UserDTO>
+    suspend fun registerSignIn(@Body jsonpath: UserDTO) : Response<LoginResponse>
 
-    @POST("member/auto-sign-in")
-    suspend fun autoSignIn(@Body jsonpath: Token ) : Response<Token>
+    @POST("secure/jwt/reissue-token")
+    @Headers("content-type: application/json")
+    suspend fun autoSignIn(@Header("authorization") jsonpath: String ) : Response<LoginResponse>
 }

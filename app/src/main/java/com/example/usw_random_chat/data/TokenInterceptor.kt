cfcbdf5ae8cd178cfc343a9a3aff31c9b45cf1db
Lee-GS <leegs4831@naver.com>
2024-05-23 02:51:25 +0900

    mod : 가이드 라인 주석처리

--- app/src/main/java/com/example/usw_random_chat/data/TokenInterceptor.kt
+++ app/src/main/java/com/example/usw_random_chat/data/TokenInterceptor.kt
@@ -25,8 +25,14 @@ class TokenInterceptor @Inject constructor(
             .build()
 
         when(chain.proceed(newRequest).code){
-            401 ->{}
-            403 ->{}
+            //리프레시 토큰을 이용해 새로운 액세스 토큰을 발급 받아야함
+            401 ->{
+
+            }
+            //새로 로그인을 시도해야함
+            403 ->{
+                //유저에게 알림 띄우고 기존의 하던 작업 전부 지우고 로그인 화면으로 돌아가야 함
+            }
         }
 
         return chain.proceed(newRequest)

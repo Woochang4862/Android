0f091ebdf146376686a959bc46d8dfe07ec49c00
Lee-GS <leegs4831@naver.com>
2024-05-25 16:08:51 +0900

    mod: 주석 삭제

--- app/src/main/java/com/example/usw_random_chat/data/TokenInterceptor.kt
+++ app/src/main/java/com/example/usw_random_chat/data/TokenInterceptor.kt
@@ -3,7 +3,6 @@ package com.example.usw_random_chat.data
 import android.util.Log
 import com.example.usw_random_chat.MainApplication
 import com.example.usw_random_chat.data.local.TokenSharedPreference
-import com.example.usw_random_chat.data.repositoryimpl.SignInRepositoryImpl_Factory
 import com.example.usw_random_chat.domain.repository.SignInRepository
 import com.example.usw_random_chat.domain.usecase.SignInUseCase
 import okhttp3.Interceptor
@@ -18,7 +17,7 @@ class TokenInterceptor @Inject constructor(
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain) : Response {
-        val accessToken = tokenSharedPreference.getToken("accessToken", "") // ViewModel에서 지정한 key로 JWT 토큰을 가져온다.
+        val accessToken = tokenSharedPreference.getToken("accessToken", "")
         val newRequest = chain.request()
             .newBuilder()
             .header("Authorization", "Bearer $accessToken") // 헤더에 authorization라는 key로 JWT 를 넣어준다.

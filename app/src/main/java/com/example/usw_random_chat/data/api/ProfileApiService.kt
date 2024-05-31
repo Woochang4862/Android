df519add6bc2079da183f4056b551602c5426a27
Lee-GS <leegs4831@naver.com>
2024-06-01 03:38:48 +0900

    mod : @PATCH -> @GET

--- app/src/main/java/com/example/usw_random_chat/data/api/ProfileApiService.kt
+++ app/src/main/java/com/example/usw_random_chat/data/api/ProfileApiService.kt
@@ -4,17 +4,19 @@ import com.example.usw_random_chat.data.dto.ProfileDTO
 import com.example.usw_random_chat.data.dto.response.ProfileResponseDTO
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ProfileApiService {
     @PATCH("secure/profile/update-my-profile") // 세부 주소
     @Headers("content-type: application/json")
     suspend fun setProfile(@Body jsonpath: ProfileDTO) : Response<ProfileDTO>
 
-    @PATCH("secure/profile/get-my-profile") // 세부 주소
+    @GET("secure/profile/get-my-profile") // 세부 주소
     @Headers("content-type: application/json")
     suspend fun getProfile(@Header("authorization") jsonpath: String ) : Response<ProfileResponseDTO>
 

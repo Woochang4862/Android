8176ae0051834559c594cab8e8f3104418314819
Lee-GS <leegs4831@naver.com>
2024-06-25 23:08:50 +0900

    add: uuid dto 추가

---
+++ app/src/main/java/com/example/usw_random_chat/data/dto/response/ResponseUUID.kt
@@ -0,0 +1,8 @@
+package com.example.usw_random_chat.data.dto.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseUUID(
+    @SerializedName("message")
+    val message : String? = "",
+    )

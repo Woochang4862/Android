92bfa5b378aa4ab7d0eb824d23a70970eb3f3c66
Lee-GS <leegs4831@naver.com>
2024-06-25 23:02:57 +0900

    mod: api 어노테이션 수정

--- app/src/main/java/com/example/usw_random_chat/data/api/SignUpApiService.kt
+++ app/src/main/java/com/example/usw_random_chat/data/api/SignUpApiService.kt
@@ -2,24 +2,26 @@ package com.example.usw_random_chat.data.api
 
 import com.example.usw_random_chat.data.dto.UserDTO
 import com.example.usw_random_chat.data.dto.response.ResponseDTO
+import com.example.usw_random_chat.data.dto.response.ResponseUUID
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SignUpApiService {
     @POST("open/member/sign-up") // 회원가입 인증 메일 전송
     @Headers("content-type: application/json")
-    suspend fun registerSignUp(@Body jsonpath: UserDTO) : Response<ResponseDTO>
+    suspend fun registerSignUp(@Body jsonpath: UserDTO) : Response<ResponseUUID>
 
     @POST("open/member/check-duplicate-account") // 아이디 중복확인
     suspend fun registerIdDoubleCheck(@Body jsonpath: UserDTO) : Response<UserDTO>
 
-    @GET("open/member/confirm-email?uuid={uuid}") // 이메일 인증확인
+    @GET("open/member/confirm-email") // 이메일 인증확인
     @Headers("content-type: application/json")
-    suspend fun checkAuthEmail(@Path("uuid") uuid : String): Response<UserDTO>
+    suspend fun checkAuthEmail(@Query("uuid") uuid : String): Response<UserDTO>
 
     @POST("open/member/reconfirm-email") // 이메일 재인증
     @Headers("content-type: application/json")
@@ -33,7 +35,4 @@ interface SignUpApiService {
     @Headers("content-type: application/json")
     suspend fun registerCheckSignUpNickName(@Body jsonpath: UserDTO): Response<UserDTO>
 
-    @GET("open/member/sign-up-finish?uuid={uuid}") // 이메일 인증확인
-    @Headers("content-type: application/json")
-    suspend fun completeSignUp(@Path("uuid") uuid : String): Response<UserDTO>
 }

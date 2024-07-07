f9d8dd72562de25416e75413c4ff1cb1dd2f033f
Lee-GS <leegs4831@naver.com>
2024-07-08 02:15:11 +0900

    mod : 자동로그인 로직에 따른 레이스 컨디션 해결중

--- app/src/main/java/com/example/usw_random_chat/data/TokenInterceptor.kt
+++ app/src/main/java/com/example/usw_random_chat/data/TokenInterceptor.kt
@@ -4,7 +4,15 @@ import android.util.Log
 import com.example.usw_random_chat.MainApplication
 import com.example.usw_random_chat.data.local.TokenSharedPreference
 import com.example.usw_random_chat.domain.repository.SignInRepository
+import com.example.usw_random_chat.domain.usecase.ProfileUseCase
 import com.example.usw_random_chat.domain.usecase.SignInUseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
@@ -13,8 +21,10 @@ import javax.inject.Inject
 
 class TokenInterceptor @Inject constructor(
     private val tokenSharedPreference: TokenSharedPreference,
+    //private val signInRepository: SignInRepository
 ) : Interceptor {
 
+    private val mutex = Mutex()
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain) : Response {
         val accessToken = tokenSharedPreference.getToken("accessToken", "")
@@ -27,10 +37,13 @@ class TokenInterceptor @Inject constructor(
         when(response.code){
             //리프레시 토큰을 이용해 새로운 액세스 토큰을 발급 받아야함
             401 ->{
-
+                CoroutineScope(Dispatchers.Default).launch {
+                    //signInRepository.autoSignIn(tokenSharedPreference.getToken("refreshToken",""))
+                }
             }
             //새로 로그인을 시도해야함
             403 ->{
+
                 //유저에게 알림 띄우고 기존의 하던 작업 전부 지우고 로그인 화면으로 돌아가야 함
             }
         }

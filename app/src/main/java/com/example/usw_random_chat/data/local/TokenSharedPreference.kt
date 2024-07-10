309366837b7135a1e7316b9f586ff60dfce54e9e
Lee-GS <leegs4831@naver.com>
2024-07-11 02:12:00 +0900

    mod : 토큰 접근 mutex로 관리, id 추가

--- app/src/main/java/com/example/usw_random_chat/data/local/TokenSharedPreference.kt
+++ app/src/main/java/com/example/usw_random_chat/data/local/TokenSharedPreference.kt
@@ -3,6 +3,8 @@ package com.example.usw_random_chat.data.local
 import android.content.Context
 import android.content.SharedPreferences
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.util.prefs.Preferences
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -11,13 +13,17 @@ import javax.inject.Singleton
 class TokenSharedPreference @Inject constructor(@ApplicationContext context: Context)  {
     private val prefs : SharedPreferences =
         context.getSharedPreferences("tokenRepo", Context.MODE_PRIVATE)
-
-    fun getToken(key: String, defValue: String): String {
-        return prefs.getString(key, defValue).toString()
+    private val mutex = Mutex()
+    suspend fun getToken(key: String, defValue: String): String {
+        return mutex.withLock {
+            prefs.getString(key, defValue).toString()
+        }
     }
 
-    fun setToken(key: String, str: String) {
-        prefs.edit().putString(key, str).apply()
+    suspend fun setToken(key: String, str: String) {
+        mutex.withLock {
+            prefs.edit().putString(key, str).apply()
+        }
     }
     fun getUUID(key: String, defValue: String): String {
         return prefs.getString(key, defValue).toString()
@@ -32,6 +38,14 @@ class TokenSharedPreference @Inject constructor(@ApplicationContext context: Con
     }
 
     fun setAccount(key: String, str: String) {
+        prefs.edit().putString(key, str).apply()
+    }
+
+    fun getID(key: String, defValue: String): String {
+        return prefs.getString(key, defValue).toString()
+    }
+
+    fun setID(key: String, str: String) {
         prefs.edit().putString(key, str).apply()
     }
 }

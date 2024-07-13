c3c00b862629ab63468a03dcfddd0d8e727a7844
Lee-GS <leegs4831@naver.com>
2024-07-13 23:45:40 +0900

    mod : accessToken 만료시 재발급

--- app/src/main/java/com/example/usw_random_chat/data/TokenInterceptor.kt
+++ app/src/main/java/com/example/usw_random_chat/data/TokenInterceptor.kt
@@ -1,27 +1,15 @@
 package com.example.usw_random_chat.data
 
-import android.util.Log
-import com.example.usw_random_chat.MainApplication
-import com.example.usw_random_chat.data.api.SignInApiService
 import com.example.usw_random_chat.data.local.TokenSharedPreference
-import com.example.usw_random_chat.data.repositoryimpl.SignInRepositoryImpl
 import com.example.usw_random_chat.domain.repository.SignInRepository
-import com.example.usw_random_chat.domain.usecase.ProfileUseCase
-import com.example.usw_random_chat.domain.usecase.SignInUseCase
 import dagger.Lazy
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
 import javax.inject.Inject
-import javax.inject.Provider
 
 
 class TokenInterceptor @Inject constructor(
@@ -29,40 +17,55 @@ class TokenInterceptor @Inject constructor(
     private val signInRepository: Lazy<SignInRepository>
 ) : Interceptor {
 
-    private val mutex = Mutex()
     @Throws(IOException::class)
-    override fun intercept(chain: Interceptor.Chain) : Response {
-        val accessToken = CoroutineScope(Dispatchers.Default).launch {
-            tokenSharedPreference.getToken("accessToken", "")
-        }
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = tokenSharedPreference.getToken("accessToken", "")
+
 
         val newRequest = chain.request()
             .newBuilder()
             .header("Authorization", "Bearer $accessToken") // 헤더에 authorization라는 key로 JWT 를 넣어준다.
             .build()
 
-        var response = chain.proceed(newRequest)
-        when(response.code){
+        val response = chain.proceed(newRequest)
+        when (response.code) {
             //리프레시 토큰을 이용해 새로운 액세스 토큰을 발급 받아야함
-            401 ->{
-                CoroutineScope(Dispatchers.IO).launch {
-                    mutex.withLock {
-                        signInRepository.get().autoSignIn(tokenSharedPreference.getToken("refreshToken",""))
+            401 -> {
+                response.close()
+                // 토큰 갱신을 동기적으로 처리
+                synchronized(this) {
+                    val updatedToken = tokenSharedPreference.getToken("accessToken", "")
+                    if (accessToken != updatedToken) {
+                        // 다른 스레드에서 이미 토큰이 갱신된 경우, 갱신된 토큰으로 요청을 다시 시도
+                        return chain.proceed(
+                            newRequest.newBuilder()
+                                .header("Authorization", "Bearer $updatedToken")
+                                .build()
+                        )
                     }
-                    val retriedRequest = chain.request()
-                        .newBuilder()
-                        .header("Authorization", "Bearer ${tokenSharedPreference.getToken("accessToken","")}") // 헤더에 authorization라는 key로 JWT 를 넣어준다.
-                        .build()
-                    response = chain.proceed(retriedRequest)
+
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        signInRepository.get().autoSignIn(tokenSharedPreference.getToken("refreshToken", ""))
+                    }
+                        // 새로운 액세스 토큰으로 요청을 다시 시도
+                        return chain.proceed(
+                            chain.request().newBuilder()
+                                .header(
+                                    "Authorization",
+                                    "Bearer ${tokenSharedPreference.getToken("accessToken", "")}"
+                                )
+                                .build()
+                        )
+                    }
+                }
+                //새로 로그인을 시도해야함
+                403 ->{
+
+                    //유저에게 알림 띄우고 기존의 하던 작업 전부 지우고 로그인 화면으로 돌아가야 함
                 }
             }
-            //새로 로그인을 시도해야함
-            403 ->{
 
-                //유저에게 알림 띄우고 기존의 하던 작업 전부 지우고 로그인 화면으로 돌아가야 함
-            }
+            return response
         }
-
-        return response
     }
-}
